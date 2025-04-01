4d9172a635d0734882f4f47caad4146b4831f562
Mohammadkaif Irshad Momin <kaifmomin2004@gmail.com>
2025-04-01 22:44:36 +0530

    LeetCode 0169

---
+++ lc0169.java
@@ -0,0 +1,31 @@
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Input: nums = [3,2,3]
+// Output: 3
+
+import java.util.Arrays;
+
+public class lc0169 {
+    public static void main(String[] args) {
+        int[] nums = { 2, 2, 1, 1, 1, 1, 2 };
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0)
+                candidate = num;
+            count += (num == candidate) ? 1 : -1;
+
+            // if (num == candidate)
+            //     count++;
+            // else
+            //     count--;
+        }
+
+        return candidate;
+    }
+}
